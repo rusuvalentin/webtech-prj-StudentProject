c10a56ea23f43600f78f01f05c968df9d89baa71
rusuvalentin <rusuvalentin95@yahoo.com>
2016-12-12 16:21:32 +0000

    controller

---
+++ app/controllers/mainController.js
@@ -0,0 +1,9 @@
+var ctrl = angular.module('students', [])
+
+ctrl.controller('mainController',function($scope,$http){
+    $http.get('/students')
+    .then((function succes(data){
+        $scope.students = data.data;
+        console.log(data);
+    })).catch((error)=>console.log("erroare controller"));
+});
--- server.js
+++ server.js
@@ -5,9 +5,39 @@ let app = express()
 
 app.use(express.static(__dirname + '/app'));
 
-app.get((req,res)=>{
-    res.status(200).send('some text')
+app.get('/students',(req,res)=>{
+    var obj = Student.findAll().then(function(students){
+        res.json(students);
+    })
+  
 })
 
+var Sequelize = require('sequelize')
+  , sequelize = new Sequelize('students', 'rusuvalentin', '', {
+      dialect: "mysql", 
+      port:    3306, 
+    });
+    
+  var Student = sequelize.define('Student', {
+  nume: Sequelize.STRING,
+  prenume: Sequelize.STRING
+});
+
+Student.sync({ force: false });
+ 
+
+ //o sa fie pe un post
+
+app.get("/students/add/:nume/:prenume",function(req,res){
+    Student.create({
+      nume:req.params.nume,
+      prenume:req.params.prenume
+      }).then(function(students){
+          res.json(students);
+      })
+  })
+app.get("*",function(req, res) {
+    res.sendfile("./app/index.html");
+})
 
 app.listen(8080);
