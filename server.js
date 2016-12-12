8f14ad5c8297ac3b034eb80aeba1a5791f3e187b
rusuvalentin <rusuvalentin95@yahoo.com>
2016-12-12 13:34:13 +0000

    install body-parser

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("asd")
+'use strict'
+
+const express = require("express")
+let app = express()
+
+
+
+app.get((req,res)=>{
+    res.status(200).send('some text')
+})
+
+
+app.listen(8080);
